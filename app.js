1ce775c1f77ac2b50ed98f9ff6da7ceedde60e38
ARadhitama <ariqradhitamaa@gmail.com>
2019-11-21 17:06:53 +0700

    nambah beberapa

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-    const axios = require('axios')
+const axios = require('axios')
+
+function getPost() {
+    axios
+        .get('localhost:9000/post?_limit=10')
+        .then(res => showPost(res))
+        .catch(err => console.log(err));
+}
+
+function addPost() {
+    axios
+        .post('localhost:9000/post/', {
+            topik : postTopic,
+            user_id : '2',
+            title : postTitle,
+            text : postText
+        })
+        .then()
+        .catch(err => console.err)
+}
+
+function showPost(res) {
+
+}
+
+function performNewPost (e) {
+    var resultElement = document.getElementById('');
+    var postTopic = document.getElementById('').value;
+    var postTitle = document.getElementById('').value;
+    var postText = document.getElementById('').value;
+}
+
+/**** EVENT LISTENER ****/ 
+
+document.getElementById('').addEventListener('submit', performNewPost);
